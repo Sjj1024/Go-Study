f198d515c17e6b22db2d546bb3d056c0322efb44
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2019-10-22 10:33:51 +0800

    Update 2-变量定义使用.py

--- 2-变量定义使用.py
+++ 2-变量定义使用.py
@@ -4,7 +4,7 @@ import "fmt"
 // 1.指定变量的类型，声明后不赋值的话，使用默认值，int默认值是0
 // 2.根据智自行判定变量类型，类型推到
 // 3.第三种：省略var，使用：=，但是要注意等号左侧的变量不应该是已经声明过的，否则报错
-// 4.多变量声明：同时声明多个变量，var n1， n2， n3 int
+// 4.多变量声明：同时声明多个变量，var n1， n2， n3 int 或者使用同时赋值，或者使用类型推导省略var
 func main() {
 	// 先定义后赋值的方式，var是variable，意思是变量，可以变的
 	var name int
@@ -18,7 +18,9 @@ func main() {
 	// 同时声明定义多个变量
 	var n1,n2,n3 int  // 使用默认值0
 	var a1,a2,a3 = 100,"xiaoming","男孩"  // 同时定义不同类型的变量
-	
+	// 同样可以使用类型推到使用多个赋值,省略了var
+	b1,b2,b3 := 18,"小红","女孩"
+
 	// 打印输出格式
 	fmt.Println("name=",name)
 	fmt.Println("age=",age)
@@ -26,8 +28,8 @@ func main() {
 	fmt.Println("gender=", gender)
 	fmt.Println("n1,n2,n3=",n1,n2,n3)
 	fmt.Println("a1,a2,a3=",a1,a2,a3)
+	fmt.Println("b1,b2,b3=",b1,b2,b3)
 }
-
 
 
 
@@ -37,4 +39,5 @@ height= 178
 gender= 男孩
 n1,n2,n3= 0 0 0
 a1,a2,a3= 100 xiaoming 男孩
-[Finished in 0.7s]
+b1,b2,b3= 18 小红 女孩
+[Finished in 0.8s]
