7200a10889dc4dbc050515f67858d095d1437fbf
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2019-10-22 14:33:45 +0800

    Create 6-布尔类型.go

---
+++ 6-布尔类型.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+
+// 布尔类型只能是true，或false，不可以使用0或者非0表示
+// 占用一个字节，适合逻辑运算 
+
+
+
+func main() {
+	// 使用布尔类型
+	num := false
+
+	fmt.Println(num)
+	
+}
+
+
+
+false
+[Finished in 0.8s]
