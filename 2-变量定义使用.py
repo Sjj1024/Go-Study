762b3e188d8b67c886b1349436e4daeaf986b777
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2019-10-22 09:58:35 +0800

    Update 2-变量定义使用.py

--- 2-变量定义使用.py
+++ 2-变量定义使用.py
@@ -1,12 +1,11 @@
 package main
 import "fmt"
-
+// 变量相当于内存中的一个房间，
 func main() {
 	var name int
 	name = 19
-	fmt.Println(name)
+	fmt.Println("name=",name)
 }
 
-
-19
-[Finished in 3.7s]
+name= 19
+[Finished in 0.8s]
