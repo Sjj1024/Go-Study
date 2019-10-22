14a594d7217834c1ba1bf231a0de9aca7c3cfa0a
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2019-10-22 10:06:46 +0800

    Update 2-变量定义使用.py

--- 2-变量定义使用.py
+++ 2-变量定义使用.py
@@ -1,15 +1,28 @@
 package main
 import "fmt"
-// 变量相当于内存中的一个房间，
+// 变量相当于内存中的一个房间，变量使用注意事项：
+// 1.指定变量的类型，声明后不赋值的话，使用默认值，int默认值是0
+// 2.根据智自行判定变量类型，类型推到
 func main() {
 	// 先定义后赋值的方式，var是variable，意思是变量，可以变的
 	var name int
 	name = 19
 	// 定义的同时赋值
 	var age int = 18
+	// 类型推到
+	var height = "178"
+	// 打印输出格式
 	fmt.Println("name=",name)
 	fmt.Println("age=",age)
+	fmt.Println("height=", height)
 }
 
+
+
+
+
+
 name= 19
-[Finished in 0.8s]
+age= 18
+height= 178
+[Finished in 0.7s]
