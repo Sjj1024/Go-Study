4f30f22f787dcecaa981a56bc32b229c3b97aef5
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2019-10-22 10:01:14 +0800

    Update 2-变量定义使用.py

--- 2-变量定义使用.py
+++ 2-变量定义使用.py
@@ -2,9 +2,13 @@ package main
 import "fmt"
 // 变量相当于内存中的一个房间，
 func main() {
+	// 先定义后赋值的方式
 	var name int
 	name = 19
+	// 定义的同时赋值
+	var age int = 18
 	fmt.Println("name=",name)
+	fmt.Println("age=",age)
 }
 
 name= 19
