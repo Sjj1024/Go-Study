c47c90fdc64d71b7c7752d9172874e9218efbd68
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2019-10-23 20:03:33 +0800

    Update 14-指针的使用.go

--- 14-指针的使用.go
+++ 14-指针的使用.go
@@ -6,14 +6,17 @@ import (
 
 // 使用指针写一个程序，获取int变量num的地址，并显示在终端
 // 将num的地址赋值给指针ptr，并通过指针ptr去修改num的值
+// 指针使用细节，使用类型：*类型
 
 func main() {
 	// 定义变量num
 
 	var num = 10
 
-	// 使用指针获取地址
-	var ptr *int = &num
+	// 使用指针获取地址：下面三种都可以
+	// var ptr *int = &num
+	// var ptr = &num
+	ptr := &num
 
 	// 通过指针修改num的值,那么这个指针指向的num值就改变了
 	*ptr = 22
