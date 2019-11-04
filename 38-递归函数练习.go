e690378f02c4eb234d6e7e17778b6bf611833805
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2019-11-04 12:02:01 +0800

    Update 38-递归函数练习.go

--- 38-递归函数练习.go
+++ 38-递归函数练习.go
@@ -25,6 +25,17 @@ func hanshu(n int) int{
 
 }
 
+// 猴子吃桃子的问题：猴子每天吃桃子的一半并多吃一个，当第十天的时候发现只有一个桃子了，
+// 问最初有多少个桃子
+func houzi(n int) int{
+	if n == 10{
+		return 1
+	}else{
+		n++
+		return 2*houzi(n) + 1
+	}
+}
+
 func main() {
 	// 使用递归求出给你一个整数的斐波那契数列的和
 	sum := feibo(4)
@@ -33,11 +44,15 @@ func main() {
 	// 使用求函数的值方程
 	han := hanshu(2)
 	fmt.Println(han)
-}
 
+	// 求出第一天一共有多少个桃子
+	tao := houzi(1)
+	fmt.Println(tao)
+}
 
 
 
 3
 7
+1023
 [Finished in 0.8s]
